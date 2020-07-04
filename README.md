4e7982ad5aae348352881ff6bff7bf49bd622b66
sparsh1801 <53870983+sparsh1801@users.noreply.github.com>
2020-07-04 19:52:34 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# c82A
+# book-santa-stage-3
+Stage -3
---
+++ components/AppDrawerNavigator.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {createDrawerNavigator}from 'react-navigation-drawer';
+import {AppTabNavigator} from './AppTabNavigator';
+import {CustomSideBarMenu}  from './CustomSideBarMenu';
+import {SettingScreen} from "../screens/SettingScreen";
+
+export const AppDrawerNavigator=createDrawerNavigator({
+    Home:{
+        screen:AppTabNavigator
+    },
+    Setting:{
+        Screen:SettingScreen
+    }
+},
+
+{
+    contentComponent:CustomSideBarMenu
+},
+{
+    initialRouteName:'Home'
+
+}
+)
